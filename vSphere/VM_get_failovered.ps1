4c12c49efea4b70a0b87289396ebe2a34e0769d0
Kedzierski Szymon <szymon.kedzierski@tieto.com>
2017-11-15 15:31:02 +0100

    lists HA failovered VMs

---
+++ vSphere/VM_get_failovered.ps1
@@ -0,0 +1,21 @@
+# Getting all VMs restarted within $days
+# Input: vCenter IP, days how long in past logs should be checked
+# Author: Szymon Kędzierski
+
+[CmdletBinding()]
+Param (
+
+# vCenter IP
+[Parameter (Mandatory=$True)] [string] $vc_IP,
+[Parameter (Mandatory=$True)] [string] $days
+
+)
+
+# Connect to the vCenter server(s) 
+$vcserver = Connect-VIServer $vc_IP 
+
+$Date=Get-Date
+Get-VIEvent -maxsamples 100000 -Start ($Date).AddDays(-$days) -type warning | Where {$_.FullFormattedMessage -match "restarted"} |select CreatedTime,FullFormattedMessage |sort CreatedTime -Descending
+
+# Disconnect from the vCenter server(s) 
+disconnect-viserver $vcserver -Confirm:$False 
